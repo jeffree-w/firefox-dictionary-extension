1d00838fca31d5bc7c6453174084ec796b82020c
Jeffree Watson <JeffreeWatson@outlook.com>
2025-03-20 22:34:22 -0400

    deleted:    background/background.js 	modified:   content/dictionary.js 	modified:   manifest.json

--- content/dictionary.js
+++ content/dictionary.js
@@ -1,15 +1,70 @@
+const getDefinition = async (word) => {
+  const dir = word.slice(0, 1);
+  const file = word.slice(0, 2);
+
+  const url = `${baseURL}/${dir}/${file}.json`;
+
+  const definitions = await fetch(url).then(res => res.json());
+
+  return definitions[word];
+}
+
+/*const getDefinition = async (word) => {
+  const dir = word.slice(0, 1);
+  const file = `${dir}.json`;
+  const filePath = browser.runtime.getURL(`api/${dir}/${file}`);
+
+  try {
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error('File not found');
+    }
+
+    const data = await response.json();
+
+    if (data[word]) {
+      const definition = data[word];
+      const wordDetails = [];
+
+      definition.etymologies.forEach((etymology) => {
+        etymology.partsOfSpeech.forEach((pos) => {
+          pos.senses.forEach((sense) => {
+            wordDetails.push({
+              partOfSpeech: pos.partOfSpeech,
+              sense: sense.sense
+            });
+          });
+        });
+      });
+
+      return {
+        word: definition.word,
+        details: wordDetails
+      };
+    } else {
+      throw new Error('Definition not found');
+    }
+  } catch (error) {
+    console.error('Error fetching definition:', error);
+    return {
+      word: word,
+      details: []
+    };
+  }
+};*/
+
+
+
 document.addEventListener('dblclick', function (event) {
   const selectedText = window.getSelection().toString().trim();
-  if (selectedText.length > 0) {
+  if (selectedText.length > 1) {
       showTooltip(event.pageX, event.pageY, selectedText);
   }
 });
 
-  
 function showTooltip(x, y, word) {
   const tooltip = document.createElement('div');
   tooltip.className = 'tooltip'; 
-  console.log(word)
 
   Object.assign(tooltip.style, {
     position: 'absolute',
@@ -24,12 +79,20 @@ function showTooltip(x, y, word) {
     zIndex: '9999',
     boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
     fontSize: '14px'
-});
+  });
 
   getDefinition(word).then((meaning) => {
-      tooltip.innerHTML = meaning;
+    const { word: wordName, details } = meaning;
+
+    let tooltipContent = `<strong>${wordName}</strong><br><br>`;
+
+    details.forEach((detail) => {
+      tooltipContent += `<strong>${detail.partOfSpeech}</strong>: ${detail.sense}<br>`;
+    });
+
+    tooltip.innerHTML = tooltipContent;
   }).catch(() => {
-      tooltip.innerHTML = 'Definition not found';
+    tooltip.innerHTML = 'Definition not found';
   });
 
   document.body.appendChild(tooltip);
@@ -39,15 +102,4 @@ function showTooltip(x, y, word) {
         tooltip.remove();
     }
   });
-}
-
-  const getDefinition = async (word) => {
-    const dir = word.slice(0, 1);
-    const file = word.slice(0, 2);
-
-    const url = `${api}/${dir}/${file}.json`;
-
-    const definitions = await fetch(url).then(res => res.json());
-
-    return definitions[word];
 }
